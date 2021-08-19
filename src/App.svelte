1ccb02113e9b647d68e641cb1e27edcaddb51187
Royal-lobster <srujangs8@gmail.com>
2021-08-19 22:26:19 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,120 @@
+<script>
+import TextArea from "./TextAreaAutosize.svelte";
+let message;
+let output = "Enter text at top";
+var handleSubmit = async (e) => {
+    if (message ?.trim().length != 0) {
+        console.log("Clicked submit")
+        let url = `https://royalobster.npkn.net/bad-words-filter/?msg=${message.split(' ').join('%20')}`
+        console.log(url)
+        let res = await fetch(url);
+        output = await res.text()
+        console.log()
+    }
+}
+</script>
+
+<main>
+	<section>
+		<h1 class="title">Bad words filter</h1>
+		<div class="inputs_container">
+			<h1>
+				Enter a Message here:
+			</h1>
+			<form class = "inputs">
+				<TextArea type="text" bind:value={message}  minRows={1} maxRows={40}/>
+				<button on:click|preventDefault={handleSubmit}>
+					Censor the Text
+				</button>
+			</form>
+		</div>
+
+		<h1>
+			Filtered Test:
+		</h1>
+		<p>
+			{output}
+		</p>
+	</section>
+	<footer>
+		Made by Srujan with Svelte
+	</footer>
+</main>
+
+<style>
+:global(body) {
+    background-color: #e64c6d;
+    position: relative;
+    margin: 0;
+    padding: 0;
+}
+
+.title{
+	font-size: 50px;
+	text-align: center;
+	color: #dbd96e;
+}
+main {
+    display: grid;
+    place-items: center;
+    min-height: 100vh;
+}
+.inputs_container{
+	background-color: #ffffff34;
+	padding: 1px 20px 15px 20px;
+
+}
+section h1 {
+    font-family: helvetica;
+    font-weight: 900;
+    color: #6b0935;
+}
+
+.inputs {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+button {
+    display: inline-block;
+    cursor: pointer;
+    padding: .7em 1.4em;
+    border-radius: .15em;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: 900;
+    color: #2a4f1d;
+    background-color: #75e84f;
+    -webkit-box-shadow: inset 0 -0.6em 0 -0.35em rgb(0 0 0 / 17%);
+    box-shadow: inset 0 -0.6em 0 -0.35em rgb(0 0 0 / 17%);
+    text-align: center;
+}
+
+p {
+    white-space: pre-wrap;
+    text-align: left;
+	width: min(400px, 90vw);
+    display: block;
+    font: normal 20px/30px Monaco, Monospace !important;
+    color: #CFDBEC;
+    background-color: #2f2f2f;
+    padding: 0 1em;
+    margin: 0;
+    overflow: auto;
+}
+
+footer {
+    background: #326fa8;
+    color: white;
+    padding: 5px 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 100vw;
+    text-align: center;
+}
+</style>
---
+++ src/TextAreaAutosize.svelte
@@ -0,0 +1,50 @@
+<script>
+	export let value = '';
+	export let minRows = 1;
+	export let maxRows;
+	
+	$: minHeight = `${1 + minRows * 1.2}em`;
+	$: maxHeight = maxRows ? `${1 + maxRows * 1.2}em` : `auto`;
+</script>
+
+<div class="container">
+	<pre
+		aria-hidden="true"
+		style="min-height: {minHeight}; max-height: {maxHeight}; width: min(400px, 90vw)"
+	>{value + '\n'}</pre>
+
+	<textarea bind:value></textarea>	
+</div>
+
+<style>
+	.container {
+		position: relative;
+	}
+	
+	pre, textarea {
+		font-family: inherit;
+		padding: 0.5em;
+		box-sizing: border-box;
+		border: 1px solid #eee;
+		line-height: 1.2;
+		overflow: hidden;
+	}
+	
+	textarea {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		resize: none;
+	}
+	textarea:focus{
+		outline: none;
+		box-shadow:
+  0 0px 2.7px rgba(0, 0, 0, 0.022),
+  0 0px 6.9px rgba(0, 0, 0, 0.031),
+  0 0px 14.2px rgba(0, 0, 0, 0.039),
+  0 0px 29.2px rgba(0, 0, 0, 0.048),
+  0 0px 80px rgba(0, 0, 0, 0.07)
+;
+	}
+</style>
